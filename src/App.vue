63ccd54d935c7bf2b3ea72d03c639ba92947d01c
penry28 <phamdinhhung28.it@gmail.com>
2022-07-30 21:01:21 +0700

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,66 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="form-wrap">
+        <div v-if="isShow" class="input-wrap">
+            <input type="text" v-model="string" />
+        </div>
+        <div class="preview" @click="isShow = !isShow">{{string}}</div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref, watch } from 'vue';
+
+const string = ref('Edit me');
+const isShow = ref(false);
+
+</script>
+<style>
+.form-wrap {
+    position: relative;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.input-wrap {
+    background-color: #5c9bb7;
+    background-image: -webkit-linear-gradient(top, #5c9bb7, #5392ad);
+    background-image: -moz-linear-gradient(top, #5c9bb7, #5392ad);
+    background-image: linear-gradient(top, #5c9bb7, #5392ad);
+    box-shadow: 0 1px 1px #ccc;
+    border-radius: 3px;
+    width: 290px;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    margin-left: -150px;
+    top: -70px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.input-wrap:after {
+    content: '';
+    position: absolute;
+    border: 6px solid #5190ac;
+    border-color: #5190ac transparent transparent;
+    width: 0;
+    height: 0;
+    bottom: -12px;
+    left: 50%;
+    margin-left: -6px;
+}
+input {
+    border: none;
+    width: 100%;
+    line-height: 34px;
+    border-radius: 3px;
+    box-shadow: 0 2px 6px #bbb inset;
+    text-align: center;
+    font-size: 16px;
+    font-family: inherit;
+    color: #8d9395;
+    font-weight: bold;
+    outline: none;
+}
+.preview {
+    font-size: 22px;
+    font-weight: bold;
+    color: #6d8088;
+    height: 30px;
+    cursor: default;
 }
 </style>
